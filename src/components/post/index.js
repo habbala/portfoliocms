56833f19ad0843be4e771a02a28fb61cc35b0020
Jesper Blomqvist <jesper.blomqvist@hotmail.com>
2018-04-04 21:40:51 +0200

    Gets posts succesfully, but cant display them

--- src/components/post/index.js
+++ src/components/post/index.js
@@ -2,12 +2,13 @@ import React, { Component } from 'react';
 import './index.css';
 
 export default class Post extends Component{
-
   render(){
+    console.log(this.props);
+
     return(
       <div>
-        <h2>{this.props.post.title}</h2>
-        <p>{this.props.post.description}</p>
+        <h2>{this.props.post}</h2>
+        <p>{this.props.post}</p>
       </div>
     )
   }
--- src/screens/home/index.js
+++ src/screens/home/index.js
@@ -4,15 +4,36 @@ import Header from '../../components/header'
 import Left from '../../components/left-side'
 import Right from '../../components/right-side'
 import postList from '../../mock/posts.js'
-import { initClient } from '../../services/contentfulClient'
+
+const contentful = require('contentful')
+
+const client = contentful.createClient({
+  space: 'q5d90ddpmtuz',
+  accessToken: '8e5c9cbf2509af385100ca4224368f3296c167ff7dccb291a5f058819c4df1f3',
+  host: 'cdn.contentful.com'
+})
 
 export default class App extends Component {
+
   constructor(props){
     super(props)
 
     this.state = {
-      posts: postList,
+      posts: [],
     }
+  }
+
+  componentDidMount(){
+    client.getEntries()
+      .then((response) => {
+        this.setState({
+          posts: response.items
+        })
+        console.log(this.state.posts)
+      })
+      .catch(function(error){
+        console.log(error);
+    });
   }
 
   render() {
